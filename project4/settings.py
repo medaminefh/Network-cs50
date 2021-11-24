2408241502e0b7cbddb972bd6b313c4533f343e4
med-amine-fh <fhal.mohamed.amine@gmail.com>
2021-11-24 22:40:34 +0100

    add the postgresql and some configs

--- project4/settings.py
+++ project4/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import Config, config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,10 +77,15 @@ WSGI_APPLICATION = 'project4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
+        'USER': config("USER"),
+        'PASSWORD': config('PASSWORD'),
     }
 }
+
 
 AUTH_USER_MODEL = "network.User"
 
