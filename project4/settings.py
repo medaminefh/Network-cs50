7c8e8b61b76fc33a034e892db93029a452f7a4d3
med-amine-fh <fhal.mohamed.amine@gmail.com>
2021-11-24 21:50:40 +0100

    add some lines to make heroku read the static files

--- project4/settings.py
+++ project4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['twitterclo.herokuapp.com', '127.0.0.1', '*']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
