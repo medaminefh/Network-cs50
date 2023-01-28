a445b19924e25e910f1b52f25c0fe3bec77f346e
Mohamed Amine Fh <fhal.mohamed.amine@gmail.com>
2023-01-28 17:05:09 +0100

    update main.yaml

--- script.py
+++ script.py
@@ -7,5 +7,7 @@ if len(argv) != 1 and argv[1] == 'dev':
     system('python3 manage.py migrate --settings=project4.settings-dev')
     system('python3 manage.py migrate --settings=project4.settings-dev --run-syncdb')
     system('python3 manage.py runserver --settings=project4.settings-dev')
+elif len(argv) != 1 and argv[1] == 'test':
+    system("python3 manage.py test --settings=project4.settings-dev")
 else:
-    print("No prod for now")
+    print("Nooo prod")
